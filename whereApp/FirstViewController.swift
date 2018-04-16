39e81193784403d1fea050f4d31087c74fe21db6
aisulubekbossynova <aisulu175@gmail.com>
2018-04-16 22:25:42 +0600

    Initial Commit

---
+++ whereApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  whereApp
+//
+//  Created by Macbook on 16.04.18.
+//  Copyright © 2018 Macbook. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
